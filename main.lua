30556239f1620df9ccd256a7c04b563df85581e6
Sebastian N <sebastian.navas@ocsbstudent.ca>
2018-02-08 14:35:38 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+--Created by: Sebastian N
+--Created on: February 8
+--
+--display a backgroud
+-----------------------------------------------------------------------------------------
+display.setDefault("background", 0, 0, 1)
+local image = display.newImageRect("bat_dead.png", 600, 600)
+image.x = 1000
+image.y = 900
+
